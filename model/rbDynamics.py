60dfe0f2124e4fb4e5ab06ee56b922936aa7aee8
wying523 <wying@umich.edu>
2014-10-19 21:12:49 -0400

    Update rbDynamics.py

--- model/rbDynamics.py
+++ model/rbDynamics.py
@@ -1,4 +1,5 @@
 import numpy as np
+from numpy.linalg import inv
 
 # Abstract Container class for planar dynamics types with a center and value
 class DynamicsPoint(object):
@@ -34,11 +35,38 @@ class MomentOfInertia(DynamicsPoint):
 	
 # Force Class
 class PointForce(DynamicsPoint):
-    def __init__(self, center, forceVector):
+    def __init__(self, center, forceVector,theta,WP):
 	DynamicsPoint.__init__(center, forceVector)
+	self.Torque = []
+        self.T_dirMatrix = []
+   	
+    def computeTorque(self):
 	
-    def computeTorque(self, axis):
-	# Implement r x F in 2D
+    	angle_si = np.arctan2([self.WP[1],  DynamicsPoint.getY], [self.WP[1],  DynamicsPoint.getX])
+    	Fx = 10*np.cos(angle_si - self.theta)
+    	Fy = 10*np.sin(angle_si - self.theta)
+    	Force  = np.array([Fx], [Fy], [0]])
+    	self.T_dirMatrix=np.array([[1., 0.5, -0.5], [0., -np.sqrt(3)/2, np.sqrt(3)/2], [0.1,0.1,0.1]])
+    	invDmatrix = numpy.linalg.inv(self.T_dirMatrix)
+    	self.Torque = invDmatrix*Force
+    	
+ 
+
+class displacement(PointForce):
+	def __init__(self,theta, WP,Rcoorp, Rcoor):
+		PointForce.__init__(center, forceVector,theta,WP)
+		self.Rnext = []
+	    
+	def nextR(self):
+		T = 0.1 #0.1 is the time increment
+		m = ? #mass
+		Rotation = np.array([[np.cos(self.theta), -1*np.sin(self.theta),0], [np.sin(self.theta), np.cos(self.theta),0],[0,0,1]])
+		ForceNew = Rotation * self.T_dirMatrix * self.Torque
+		Vp = (self.Rcoor - self.Rcoorp)/T 
+		Vcurrent = Vp*0.5 + np.array([[ForceNew[0]*T/(2*m)], [ForceNew[1]*T/(2*m)]])
+		self.Rnext = Rcoor+Vcurrent*T
+		
+	
 "
 Planar Rigid Body Dynamics Class based on NumPy/SciPy ODE
 "
@@ -75,4 +103,6 @@ class RBDynamics(object):
 	
     # Instantaneous center of rotation
     def computeCoR(self):
+    
+
 	
