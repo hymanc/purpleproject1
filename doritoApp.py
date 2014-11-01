36f8d897bae1ce0c3e867d8fde399cc7e7e09b16
Cody Hyman <hymanc@umich.edu>
2014-10-31 21:07:40 -0400

    Updates to DoritoApp

--- doritoApp.py
+++ doritoApp.py
@@ -8,6 +8,13 @@ from fixed_vision.visionPlan import VisionPlan
 from fixed_vision.sensorPlan import SensorPlan
 from doritoControl.doritoMotion import DoritoMotion
 
+# Dorito state enumeration class
+class DoritoState(object):
+    UNKNOWN = 'UNKNOWN'
+    SETUP = 'SETUP'
+    RUNNING = 'RUNNING'
+    FINISHED = 'FINISHED'
+    
 # Top Level Dorito Robot Class
 class DoritoApp( JoyApp ):
     
@@ -19,9 +26,12 @@ class DoritoApp( JoyApp ):
     # Initialization
     def onStart( self ):
 	print '\n===== Doritos Xtreme Nacho Cheese Robot Command and Control v99.37 =====\n\n'
-	#TODO: Control plan
+	
+	# Attempt to start vision system
 	self.vplan = VisionPlan(self, camera=ncam)
 	self.vplan.start()
+	
+	# Attempt to start drive system
 	self.servoErrorFlag = False
 	try:
 	    self.drive = DoritoMotion() #Initialize Motion DoritoDriver
@@ -29,15 +39,22 @@ class DoritoApp( JoyApp ):
 	    print '\n*******\nWARNING: Could not connect to drive servos!!!!!\n*******'
 	    self.servoErrorFlag = True
 	    
+	# Attempt to start sensor system
 	print 'Starting Sensor Plan\n\n'
 	self.sensor = SensorPlan(self)
 	self.sensor.start()
 	
+	# Set timer for events
+	self.timeForControl = self.onceEvery(0.1)
+	
     # Top Level Event Handler (Key commands?)
     def onEvent( self , evt ):
 	#print 'Event Occurred', str(evt)
-	if(evt.type == KEYDOWN):
-	    print 'Main Keydown event'
+	if self.timeForControl(): 
+	    print 'Handling Control Update'
+	elif(evt.type == KEYDOWN):
+	    print 'Key Pressed'
+	    # Get key and check for start/stop condition
 	return JoyApp.onEvent(self,evt)
 	
 	
